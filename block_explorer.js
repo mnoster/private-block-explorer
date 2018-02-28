4610b96f23ea2858ad3f3c64d44795721bc99ad9
Nicholas Porter <nicholas@metax.io>
2018-02-27 23:58:49 -0800

    phase one complete all fields dynamically pulled from priv evm node

--- block_explorer.js
+++ block_explorer.js
@@ -4,9 +4,25 @@ let events
 let decodedEvents
 let $events
 let $submit
+let overview = {
+	$to : '',
+	$from : '',
+	$inputData: '',
+	$eventType: '',
+	$timestamp: '',
+	$block: ''
+}
+
+
 const loadHandlers = () => {
 	$events = document.getElementById("events")
 	$submit = document.getElementById("submit-btn")
+	overview.$to = document.getElementById("to")
+	overview.$from = document.getElementById("from")
+	overview.$inputData = document.getElementById("input-data")
+	overview.$eventType = document.getElementById("event-type")
+	overview.$timestamp = document.getElementById("timestamp")
+	overview.$block = document.getElementById("block")
 	$submit.addEventListener("click", function(e){
 		$events.innerHTML = lookup()
 	})
@@ -14,23 +30,43 @@ const loadHandlers = () => {
 
 const getEventHistory = async () => {
 	let res = await axios.post("http://54.214.107.168:8000/getEventHistory")
-	// $events.innerHTML = JSON.stringify(res)
 	events = res.data.events
 	decodedEvents = res.data.decodedEvents
 	console.log(res)
 }
 
-const lookup = () => {
+const lookup = async () => {
+
 	let tx = document.getElementById("tx").value
-	for(let i = 0; i < events.length; i++){
-		if(events[i].transactionHash === tx){
-			return [JSON.stringify(events[i]) , decodedEvents[i]]
-		}
+	try{
+		let res =  await axios.post("http://54.214.107.168:8000/getTransaction", {tx: tx})
+		console.log("res: ", res)
+		populateFields(res.data)
+	}catch(error){
+		alert("Error looking up transaction")
 	}
-	console.log("tx: " + tx + ' not found')
 	return 
 }
 
+const populateFields = (data) => {
+	overview.$to.innerHTML = data.to
+	overview.$from.innerHTML = data.from
+	overview.$inputData.innerHTML = matchEventData(data.transactionHash)
+	overview.$eventType.innerHTML = matchEventData(data.transactionHash)[0]
+	overview.$timestamp.innerHTML = data.timestamp
+	overview.$block.innerHTML = data.blockNumber
+	$events.innerHTML = JSON.stringify(data.logs[0])
+}
 
-loadHandlers()
+const matchEventData = (tx) => {
+	for(let i = 0; i < events.length; i++){
+		if(events[i].transactionHash === tx){
+			let result = decodedEvents[i]
+			return result
+		}
+	}
+	console.log("tx: " + tx + ' not found')
+}
+
 getEventHistory()
+loadHandlers()
